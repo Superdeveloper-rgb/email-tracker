fe225e96623c429e6e065741abca0c26f7ea2897
Superdeveloper-rgb <62853708+Superdeveloper-rgb@users.noreply.github.com>
2022-11-15 20:47:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const http = require('http');
+const url = require('url');
+
+
+const requestListener = async function (req, res) {
+  const queryObject = url.parse(req.url, true).query;
+
+  if (queryObject.id) {
+    const result = await prisma.email.upsert({
+      create: { opens: 1 },
+      update: { opens: { increment: 1 } },
+      where: { id: parseInt(queryObject.id) },
+    });
+    let img = Buffer.from("iVBO…AAAYAAjCB0C8AAAAASUVORK5CYII=", 'base64');
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+    res.end(img);
+  } else {
+    const result = await prisma.email.findMany();
+    console.log(result);
+    if (result) {
+      res.writeHeader(200, { "Content-Type": "text/html" });
+      let html = "<ul>";
+      result.forEach(item => {
+        html += `<li>Email ${item.id} has ${item.opens} opens</li>`;
+      });
+      html += "</ul>";
+      console.log(html);
+      res.write(html);
+      res.end();
+    }
+  }
+}
+
+const server = http.createServer(requestListener);
+server.listen(80);
